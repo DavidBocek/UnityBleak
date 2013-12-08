b7f3144f36546a9b27041ec6a92e1ebf659b3b1f
DavidBocek <dmbocek@mit.edu>
2013-12-08 10:47:43 -0500

    more layers and parallax code added

---
+++ UnityBleak/Assets/Lib/Scripts/Camera Scripts/ParallaxScrollingElement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxScrollingElement : MonoBehaviour {
+
+	public float scrollAmountX;
+	public float scrollAmountY;
+
+	private Vector3 originalLocation;
+	
+	void Start(){
+		originalLocation = new Vector3(transform.position.x,transform.position.y,transform.position.z);
+		Messenger.AddListener<Vector2>("parallaxUpdate", UpdateParallaxMovement);
+	}
+
+	void UpdateParallaxMovement(Vector2 cameraPosition){
+		Vector3 deltaPosition = new Vector3(cameraPosition.x*scrollAmountX,cameraPosition.y*scrollAmountY,0.0f);
+		Vector3 orig = new Vector3(-originalLocation.x,-originalLocation.y,originalLocation.z);
+		transform.position = originalLocation + deltaPosition;
+	}
+}
--- UnityBleak/Assets/Lib/Scripts/Camera Scripts/Smooth2DCamera.cs
+++ UnityBleak/Assets/Lib/Scripts/Camera Scripts/Smooth2DCamera.cs
@@ -6,17 +6,27 @@ public class Smooth2DCamera : MonoBehaviour {
     public float dampTime = 0.15f;
     private Vector3 velocity = Vector3.zero;
     public Transform target;
- 
+ 	
+	public Vector2 lowerLeft = new Vector2(0.0f,0.0f);
+	public Vector2 upperRight;
+
     // Update is called once per frame
     void Update () 
     {
-       if (target)
-       {
-         Vector3 point = camera.WorldToViewportPoint(target.position);
-         Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
-         Vector3 destination = transform.position + delta;
-         transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
-       }
- 
-    }
+       	if (target)
+       	{
+        	 Vector3 point = camera.WorldToViewportPoint(target.position);
+        	 Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+        	 Vector3 destination = transform.position + delta;
+        	 transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+       	}
+		Vector3 temp = new Vector3(transform.position.x,transform.position.y,transform.position.z);
+		if (transform.position.x < lowerLeft[0]){ temp.x = lowerLeft[0]; }
+		else if (transform.position.x > upperRight[0]){ temp.x = upperRight[0]; }
+		if (transform.position.y < lowerLeft[1]){ temp.y = lowerLeft[1]; }
+		else if (transform.position.y > upperRight[1]){ temp.y = upperRight[1]; }
+		transform.position = temp;
+		Vector2 cameraPosition = new Vector2(transform.position.x,transform.position.y);
+		Messenger.Broadcast<Vector2>("parallaxUpdate",cameraPosition);
+	}
 }
