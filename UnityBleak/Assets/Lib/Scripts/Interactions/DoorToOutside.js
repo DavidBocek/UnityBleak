889b5c779c07415c61544454fce7f88883984c44
B1ueman123 <b1ueman123@yahoo.com>
2013-12-18 23:14:13 -0500

    Odd Lerp to level that doesnt use parallax

---
+++ UnityBleak/Assets/Lib/Scripts/Interactions/DoorToOutside.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+private var OnDoor: boolean = false;
+
+function Start () {
+
+}
+
+function OnTriggerEnter2D(other: Collider2D)
+{
+	OnDoor = true;
+}
+
+function Update () 
+{
+	if(OnDoor == true)
+	{
+		Application.LoadLevel("Scene 1-1");
+	}
+}
