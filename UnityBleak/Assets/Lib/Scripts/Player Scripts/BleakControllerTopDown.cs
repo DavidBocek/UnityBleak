2c21f53747d6b9c0318207c2b47899aad5dedc5f
B1ueman123 <b1ueman123@yahoo.com>
2013-12-29 21:25:02 -0500

    Added bleak movement but not idles

--- UnityBleak/Assets/Lib/Scripts/Player Scripts/BleakControllerTopDown.cs
+++ UnityBleak/Assets/Lib/Scripts/Player Scripts/BleakControllerTopDown.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class BleakControllerTopDown : MonoBehaviour {
@@ -7,6 +7,10 @@ public class BleakControllerTopDown : MonoBehaviour {
 
 	private CircleCollider2D collider;
 	private Rigidbody2D rigidBody;
+	private SkeletonAnimation skelAnim;
+	public GameObject skelAnimObj;
+	public float changeSpeed;
+	public float lagSpeed;
 
 	private Vector2 velocity;
 	private Vector2 position;
@@ -14,6 +18,7 @@ public class BleakControllerTopDown : MonoBehaviour {
 	void Start(){
 		rigidBody = GetComponent<Rigidbody2D>();
 		collider = GetComponent<CircleCollider2D>();
+		skelAnim = skelAnimObj.GetComponent<SkeletonAnimation>();
 	}
 
 	private float dt;
@@ -25,10 +30,67 @@ public class BleakControllerTopDown : MonoBehaviour {
 		float xInput = Input.GetAxis("Horizontal");
 		float yInput = Input.GetAxis("Vertical");
 
-		if (xInput != 0 || yInput != 0){
-			velocity.x = xInput;
-			velocity.y = yInput;
-			velocity.Normalize();
+		if (xInput != 0 || yInput != 0)
+		{
+			velocity.x = xInput * walkSpeed;
+			velocity.y = yInput * walkSpeed;
+			//velocity.Normalize();
+			if((xInput > changeSpeed) && (yInput > -lagSpeed) && (yInput < lagSpeed))
+			{
+				if (skelAnim.state.ToString()!="walkingRight")
+				{
+					skelAnim.state.SetAnimation(0,"walkingRight",true);
+				}
+			}
+			if((xInput>changeSpeed)&&(yInput>changeSpeed))
+			{
+				if (skelAnim.state.ToString()!="walkingUp-right")
+				{
+					skelAnim.state.SetAnimation(0,"walkingUp-right",true);
+				}
+			}
+			if((xInput < -changeSpeed)&&(yInput > -lagSpeed) && (yInput < lagSpeed))
+			{
+				if (skelAnim.state.ToString()!="walkingLeft")
+				{
+					skelAnim.state.SetAnimation(0,"walkingLeft",true);
+				}
+			}
+			if((xInput<-changeSpeed)&&(yInput>changeSpeed))
+			{
+				if (skelAnim.state.ToString()!="walkingUp-left")
+				{
+					skelAnim.state.SetAnimation(0,"walkingUp-left",true);
+				}
+			}
+			if((yInput > changeSpeed) && (xInput > -lagSpeed) && (xInput < lagSpeed))
+			{
+				if (skelAnim.state.ToString()!="walkingUp")
+				{
+					skelAnim.state.SetAnimation(0,"walkingUp",true);
+				}
+			}
+			if((xInput<-changeSpeed)&&(yInput<-changeSpeed))
+			{
+				if (skelAnim.state.ToString()!="walkingDown-left")
+				{
+					skelAnim.state.SetAnimation(0,"walkingDown-left",true);
+				}
+			}
+			if((yInput < -changeSpeed) && (xInput > -lagSpeed) && (xInput < lagSpeed))
+			{
+				if (skelAnim.state.ToString()!="walkingDown")
+				{
+					skelAnim.state.SetAnimation(0,"walkingDown",true);
+				}
+			}
+			if((xInput>changeSpeed)&&(yInput<-changeSpeed))
+			{
+				if (skelAnim.state.ToString()!="walkingDown-right")
+				{
+					skelAnim.state.SetAnimation(0,"walkingDown-right",true);
+				}
+			}
 		}
 
 		UpdatePositionChangeNormal(dt);
