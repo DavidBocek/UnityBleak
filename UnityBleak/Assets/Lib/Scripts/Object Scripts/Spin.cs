1fc35146589c1664911aacc83251c5d5bcc9a07e
B1ueman123 <b1ueman123@yahoo.com>
2014-01-13 18:53:56 -0500

    counter anims placed

---
+++ UnityBleak/Assets/Lib/Scripts/Object Scripts/Spin.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour
+{
+	public float speed = 10f;
+	
+	
+	void Update ()
+	{
+		transform.Rotate(new Vector3(0,0,1 * speed * Time.deltaTime));
+	}
+}
