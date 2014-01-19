bc7627588d7820e07df5df79276cfdf13283a697
B1ueman123 <b1ueman123@yahoo.com>
2014-01-18 21:05:03 -0500

    pickup adds

--- UnityBleak/Assets/Lib/Scripts/Object Scripts/Pickup.cs
+++ UnityBleak/Assets/Lib/Scripts/Object Scripts/Pickup.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class Pickup : MonoBehaviour {
 	
 	public int type;
+	public AudioClip taken;
 
 	public void Grab(GameObject obj){
+		AudioSource.PlayClipAtPoint(taken, transform.position,1);
 		gameObject.SetActive(false);
 		obj.GetComponent<BleakInventoryManager>().GetPickup(type);
 	}
+
 }
