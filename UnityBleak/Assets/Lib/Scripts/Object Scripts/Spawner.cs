dd36306d849376c9f4ea07f763f780455b874f2d
B1ueman123 <b1ueman123@yahoo.com>
2014-01-22 23:05:58 -0500

    Acid drops are in but need to be destroyed

---
+++ UnityBleak/Assets/Lib/Scripts/Object Scripts/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+	public float spawnTime = 5f;		// The amount of time between each spawn.
+	public float spawnDelay = 3f;		// The amount of time before spawning starts.
+	public GameObject drop;
+
+	void Start ()
+	{
+		InvokeRepeating("Spawn", spawnDelay, spawnTime);
+	}
+	
+	
+	void Spawn ()
+	{
+		Instantiate(drop, transform.position, transform.rotation);
+	}
+}
