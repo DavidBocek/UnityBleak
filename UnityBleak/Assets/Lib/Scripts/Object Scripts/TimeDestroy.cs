fbd020cf40083f894f0ac584acc97f57fe7af4a5
B1ueman123 <b1ueman123@yahoo.com>
2014-01-23 20:37:48 -0500

    Timed destroy put in

---
+++ UnityBleak/Assets/Lib/Scripts/Object Scripts/TimeDestroy.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeDestroy : MonoBehaviour {
+	public float TimeDelay = 2;
+	// Update is called once per frame
+	void Update () 
+	{
+		Destroy(gameObject, TimeDelay);
+	}
+}
