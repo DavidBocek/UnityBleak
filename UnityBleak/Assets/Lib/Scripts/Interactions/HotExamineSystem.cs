85ad6a513ad6704291af712398f8a3582f78a2e8
B1ueman123 <b1ueman123@yahoo.com>
2014-02-17 07:15:46 -0500

    Added the examining triggers in the first scene

---
+++ UnityBleak/Assets/Lib/Scripts/Interactions/HotExamineSystem.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HotExamineSystem : MonoBehaviour 
+{
+	private bool bShowGUI = false;
+	public AudioClip examine;
+	private bool hasPlayed = false;
+	public string dialogue;
+	//public float textWidth = 200;
+	//public float textHeight = 40;
+	public GUIText thewords;
+	public GameObject indicator;
+	
+	void OnTriggerEnter2D(Collider2D other) 
+	{ 
+		bShowGUI = true;
+		indicator.gameObject.renderer.material.color = Color.red;
+		//thewords.enabled = true;
+	}
+	void OnTriggerExit2D(Collider2D other) 
+	{ 
+		bShowGUI = false;
+		indicator.gameObject.renderer.material.color = Color.yellow;
+		thewords.enabled = false;
+		audio.Stop();
+	}
+	void OnGUI() 
+	{ 
+		if(bShowGUI) //DrawButton/Label etc
+		{
+			thewords.enabled = true;
+			thewords.text = dialogue;
+			//GUI.Label (new Rect (Screen.width/2-textWidth/2, Screen.height/4, textWidth, textHeight), dialogue);
+			if(Input.GetKeyDown(KeyCode.E))
+			{
+				if(hasPlayed == false)
+				{
+					hasPlayed = true;
+					audio.clip = examine;
+					audio.Play();
+				}
+				else
+					hasPlayed = false;
+			}
+		}
+	}
+
+}
---
+++ UnityBleak/Assets/Lib/Scripts/Interactions/WarmWarning.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarmWarning : MonoBehaviour 
+{
+	private bool bShowGUI = false;
+	public GUIText thewords;
+	public GameObject indicator;
+	//public float textWidth = 4;
+	//public float textHeight = 20;
+	
+	void OnTriggerEnter2D(Collider2D other) 
+	{ 
+		bShowGUI = true;
+		indicator.gameObject.renderer.material.color = Color.yellow;
+		thewords.enabled = true;
+	}
+	void OnTriggerExit2D(Collider2D other) 
+	{ 
+		bShowGUI = false;
+		indicator.gameObject.renderer.material.color = Color.clear;
+		thewords.enabled = false;
+	}
+	void OnGUI() 
+	{ 
+		if(bShowGUI) //DrawButton/Label etc
+		{
+			thewords.text = "Warm";
+			//GUI.Label (new Rect (Screen.width/2-textWidth/2, Screen.height/4, textWidth, textHeight), "Warm");
+		}
+	}
+	/*void Update()
+	{
+		if(bShowGUI)
+		{
+			indicator.renderer.material.color = new Color(1,0.5,0.4);
+		}
+		else
+			indicator.renderer.material.color = new Color(148,191,146);
+	
+	}*/
+}
