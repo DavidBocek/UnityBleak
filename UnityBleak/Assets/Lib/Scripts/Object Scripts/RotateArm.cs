39f6e92eac2fbdf5a782487f4eda442aa6dd1b24
B1ueman123 <b1ueman123@yahoo.com>
2014-03-09 22:11:40 -0400

    Pendulum Killer added to 1-2

---
+++ UnityBleak/Assets/Lib/Scripts/Object Scripts/RotateArm.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateArm : MonoBehaviour 
+{
+	// Interpolates rotation between the rotations
+	// of from and to.
+	// (Choose from and to not to be the same as 
+	// the object you attach this script to)
+	public Transform from;
+	public Transform to;
+	public int speed = 1;
+	void  Update ()
+	{
+		transform.rotation = Quaternion.Lerp (from.rotation, to.rotation, Time.time * speed);
+	}
+}
