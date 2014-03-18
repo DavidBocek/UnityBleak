973a9a8f3249d21419170f2f003982fb17505363
B1ueman123 <b1ueman123@yahoo.com>
2014-03-17 23:33:39 -0400

    New bleak art

--- UnityBleak/Assets/Lib/Scripts/World Scripts/EnterNextLevel.cs
+++ UnityBleak/Assets/Lib/Scripts/World Scripts/EnterNextLevel.cs
@@ -5,7 +5,7 @@ public class EnterNextLevel : MonoBehaviour {
 
 	public string nextLevelEnterPointTag;
 	public string nextLevel;
-
+	
 	void OnTriggerEnter2D(Collider2D other){
 		if (other.gameObject.CompareTag("Player")){
 			GameObject.FindWithTag("LevelManager").GetComponent<LevelManager>().levelEnterLocation = nextLevelEnterPointTag;
